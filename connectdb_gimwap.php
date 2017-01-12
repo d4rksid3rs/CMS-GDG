91a76c650b77aa6ed9c492422727408d9d7f63d9
d4rksid3rs <gaproghe@gmail.com>
2017-01-12 19:36:39 +0700

    connect gim_wap

--- connectdb_gimwap.php
+++ connectdb_gimwap.php
@@ -1,7 +1,11 @@
 <?php
-$host="localhost:3306";
-$user="root";
-$password="";
+//$host="localhost:3306";
+//$user="root";
+//$password="";
+
+$host="local.db:3306";
+$user="gdg";
+$password="gfsd34312saAAdaq";
 
 mysql_connect($host,$user,$password);
 mysql_select_db("gim_wap");
