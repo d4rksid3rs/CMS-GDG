7ce21c998be8642e09966b908516e6c224366f00
d4rksid3rs <gaproghe@gmail.com>
2017-01-21 19:11:50 +0700

    NPU fix

--- API/statNPU.php
+++ API/statNPU.php
@@ -7,8 +7,12 @@ if ($_GET['fromDate'] && $_GET['toDate']) {
     $toDate = $_GET['toDate'] . " 23:59:59";
     try {
 //        $sql = "SELECT * FROM `koin_deduct` WHERE `date_created` BETWEEN '$fromDate' AND '$toDate' AND `return_code` = 1";                
-        $sql = "select u.*, auv.sum_money from user u join auth_user_vip auv on u.id = auv.auth_user_id "
-                . "where (u.date_created  >= '{$fromDate}' AND u.date_created <= '{$toDate}' AND auv.sum_money > 0)";
+        $sql = "select u.username, u.screen_name, auv.sum_money, b.sms, c.card, d.iap from user u 
+left join (select username, sum(money) as sms from log_nap_koin where type = 1 group by username) b on u.username = b.username
+left join (select username, sum(money) as card from log_nap_koin where type = 2 group by username) c on u.username = c.username
+left join (select username, sum(money) as iap from log_nap_koin where type = 4 group by username) d on u.username = d.username
+join auth_user_vip auv on u.id = auv.auth_user_id 
+where (u.date_created >= '{$fromDate}' AND u.date_created <= '{$toDate}' AND auv.sum_money > 0)";
         $sql_count = "select count(u.username) count from user u join auth_user_vip auv on u.id = auv.auth_user_id "
                 . "where (u.date_created  >= '{$fromDate}' AND u.date_created <= '{$toDate}' AND auv.sum_money > 0)";
         $total = $db->prepare($sql_count);
@@ -20,7 +24,8 @@ if ($_GET['fromDate'] && $_GET['toDate']) {
         $resultData = array();
         $html ="<b style='color:#fff;'>Tổng User: ".$result['count']."</b><br />";
         $html .= "<table width='100%'><tr style='background-color: rgb(255, 255, 255);text-align:center;font-weight:bold;'>";
-        $html .= "<td>STT</td><td>Username</td><td>Screen Name</td><td>Mobile</td><td>CP</td><td>Tổng nạp</td><td>Ngày giờ Đăng ký</td></tr>";
+        $html .= "<td>STT</td><td>Username</td><td>Screen Name</td><td>Mobile</td><td>CP</td>"
+                . "<td>SMS</td><td>CARD</td><td>IAP</td><td>Tổng nạp</td><td>Ngày giờ Đăng ký</td></tr>";
         $i = 0;
         foreach ($db->query($sql) as $row) {
             $i+=1;
@@ -28,10 +33,13 @@ if ($_GET['fromDate'] && $_GET['toDate']) {
             $html .= "<tr style='background-color: rgb(" . ($i % 2 > 0 ? "204,204,204" : "255, 255, 255") . ");text-align:center;'>";
             $html .= "<td width='5%'>" . $i . "</td>";
             $html .= "<td width='10%'>" . $row['username'] . "</td>";
-            $html .= "<td width='15%'>" . $row['screen_name'] . "</td>";
+            $html .= "<td width='10%'>" . $row['screen_name'] . "</td>";
             $html .= "<td width='10%'>" . $row['mobile'] . "</td>";
             $html .= "<td width='5%'>" . $row['cp'] . "</td>";
-            $html .= "<td width='15%'>" . number_format($row['sum_money']) . "</td>";
+            $html .= "<td width='10%'>" . number_format($row['sms']) . "</td>";
+            $html .= "<td width='10%'>" . number_format($row['card']) . "</td>";
+            $html .= "<td width='10%'>" . number_format($row['iap']) . "</td>";
+            $html .= "<td width='10%'>" . number_format($row['sum_money']) . "</td>";
             $html .= "<td width='10%'>" . $row['date_created'] . "</td>";
             $html .= "</tr>";
         }
