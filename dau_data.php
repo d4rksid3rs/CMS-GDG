6bfbcfc07eb02e9f62b5f8f6a27061e51124bee8
d4rksid3rs <gaproghe@gmail.com>
2017-02-08 19:09:30 +0700

    Fix performance DAU Real Time

--- dau_data.php
+++ dau_data.php
@@ -6,108 +6,89 @@ $end_date = date('Y-m-d');
 
 //$date_start = date ("Y-m-d", strtotime("-7 day", strtotime($end_date)));
 //$end_date = date ("Y-m-d", strtotime("-1 day", strtotime($end_date)));
-
+$today = date("Y-m-d");
 $date_start = $_GET['fromDate'];
 $end_date = $_GET['toDate'];
 
 $start_date = $date_start;
 
 $week = array();
-while (strtotime($date_start) <= strtotime($end_date)) {               
-	$week[]= $date_start;
-        $date_start = date ("Y-m-d", strtotime("+1 day", strtotime($date_start)));  
+while (strtotime($date_start) <= strtotime($end_date)) {
+    $week[] = $date_start;
+    $date_start = date("Y-m-d", strtotime("+1 day", strtotime($date_start)));
 }
 //TOP CP trong vong 1 tuan
-//$sql = "select name1, sum(dau) as sum_dau from active_user_detail "
-//        . "where type=2 and date_login >= '{$start_date}' AND date_login <= '{$end_date}' group by name1 order by sum_dau desc";
+$sql = "select name1, sum(dau) as sum_dau from active_user_detail "
+        . "where type=2 and date_login >= '{$start_date}' AND date_login <= '{$end_date}' group by name1 order by sum_dau desc";
 //
 ////die($sql);
-//$cps = array();
+$cps = array();
 ////$cpname = array("x");
-//foreach ($db->query($sql) as $row) {
-//	$cps[] = "'".$row['name1']."'";
-//	//$cpname[] = $row['name1'];
-//}
+foreach ($db->query($sql) as $row) {
+    $cps[] = "'" . $row['name1'] . "'";
+    //$cpname[] = $row['name1'];
+}
 //
 ////$cpname = asort($cpname);
 //
-//$cplist = implode(",",$cps);
+$cplist = implode(",", $cps);
 
 //var_dump($cps);
 //
-//$sql1 = "select date_login,name1,dau from active_user_detail 
-//			where type=2
-//					and name1 IN ({$cplist}) 
-//					and date_login >= '{$start_date}' AND date_login <= '{$end_date}'
-//				group by name1,date_login 
-//				order by name1"; //de duyet data dua vao mang cho de
-                                        
-$sql1 = "select count(*) as dau, cp as name1, date(date_created) as date_login "
-        . "from user where date(date_created) >= '{$start_date}' AND date(date_created) <= '{$end_date}' group by cp, date(date_created)";
-				
+$sql1 = "select date_login,name1,dau from active_user_detail 
+			where type=2
+					and name1 IN ({$cplist}) 
+					and date_login >= '{$start_date}' AND date_login <= '{$end_date}'
+				group by name1,date_login 
+				order by name1"; //de duyet data dua vao mang cho de
 $data = array();
 $cpname = array();
 $lastCPName = "";
 foreach ($db->query($sql1) as $row) {
-	$data[] = $row;
-	if ($row['name1']!=$lastCPName){
-		$lastCPName = $row['name1'];
-		$cpname[] = $lastCPName;
-	}
-	//echo $row["date_login"];
-	//$data[] = array($row["date_login"],$row["name1"],$row["dau"]);
-
+    $data[] = $row;
+    if ($row['name1'] != $lastCPName) {
+        $lastCPName = $row['name1'];
+        $cpname[] = $lastCPName;
+    }
+    //echo $row["date_login"];
+    //$data[] = array($row["date_login"],$row["name1"],$row["dau"]);
 }
-array_unshift($cpname,"x");
-
+array_unshift($cpname, "x");
+if (in_array($today, $week)) {
+    $my_file = file_get_contents("./dau/rt_reg_cp");
+    $jsonData = json_decode($my_file, true);
+    foreach ($jsonData as $val) {
+        array_push($data, $val);
+    }    
+}
 //echo json_encode($data);
-//var_dump($data);
 
 //asort($cpname);
 
 $table = array($cpname);
 $sizeOfCP = count($cpname);
 
-foreach ($week as $day){
-		//
-		//echo $day."<br />";
-		//echo $name."<br />";
-		$table_row = array();
-		$table_row[] = formatdate($day);
-	foreach ($cpname as $name){
-		if ($name == "x") continue;
-		$found = 0;
-		foreach ($data as $row){
-			
-			//echo $row['name1']."<br />";
-				if ($row['date_login'] == $day && $row['name1']==$name){
-					//echo "trong ".$day."<br />";
-					$table_row[] =  intval($row['dau']);
-					$found++;
-					break;
-					
-				} 
-			
-			
-			//var_dump($day);
-			//var_dump($row);
-			
-		}
-		if ($found==0) $table_row[] =0;
-		
-	
-		
-		
-			
-		//}
-	}
-	//if ($found>0) 
-	$table[] = $table_row;
+foreach ($week as $day) {
+    $table_row = array();
+    $table_row[] = formatdate($day);
+    foreach ($cpname as $name) {
+        if ($name == "x") {
+            continue;
+        }
+        $found = 0;
+        foreach ($data as $row) {
+            if ($row['date_login'] == $day && $row['name1'] == $name) {
+                $table_row[] = intval($row['dau']);
+                $found++;
+                break;
+            }
+        }
+        if ($found == 0) {
+            $table_row[] = 0;
+        }
+    }
+    $table[] = $table_row;
 }
-//echo json_encode($table);
-
-//$xAxis = "CP,".$cps;
-//die();
 ?>
 
 <!--
@@ -117,34 +98,34 @@ Apache license (http://www.apache.org/licenses/LICENSE-2.0.html)
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>
-      Google Visualization API Sample
-    </title>
-    <script type="text/javascript" src="//www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load('visualization', '1', {packages: ['corechart']});
-    </script>
-    <script type="text/javascript">
-      function drawVisualization() {
-        // Create and populate the data table.
-        var data = google.visualization.arrayToDataTable(<?php echo json_encode($table); ?>);
-      
-        // Create and draw the visualization.
-        new google.visualization.LineChart(document.getElementById('visualization')).
-            draw(data, {curveType: "function",
-                        width: 500, height: 300,
-                        vAxis: {maxValue: 10, viewWindowMode: "explicit", viewWindow:{ min: 0 }}}
-                );
-      }
-      
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <title>
+            Google Visualization API Sample
+        </title>
+        <script type="text/javascript" src="//www.google.com/jsapi"></script>
+        <script type="text/javascript">
+            google.load('visualization', '1', {packages: ['corechart']});
+        </script>
+        <script type="text/javascript">
+            function drawVisualization() {
+                // Create and populate the data table.
+                var data = google.visualization.arrayToDataTable(<?php echo json_encode($table); ?>);
 
-      google.setOnLoadCallback(drawVisualization);
-    </script>
-  </head>
-  <body style="font-family: Arial;border: 0 none;">
-    <div id="visualization" style="width: 500px; height: 300px;"></div>
-  </body>
+                // Create and draw the visualization.
+                new google.visualization.LineChart(document.getElementById('visualization')).
+                        draw(data, {curveType: "function",
+                            width: 500, height: 300,
+                            vAxis: {maxValue: 10, viewWindowMode: "explicit", viewWindow: {min: 0}}}
+                        );
+            }
+
+
+            google.setOnLoadCallback(drawVisualization);
+        </script>
+    </head>
+    <body style="font-family: Arial;border: 0 none;">
+        <div id="visualization" style="width: 500px; height: 300px;"></div>
+    </body>
 </html>	
 ​
--- dauversion1.php
+++ dauversion1.php
@@ -9,6 +9,7 @@ $end_date = date('Y-m-d');
 //$date_start = date ("Y-m-d",strtotime("yesterday"));
 //$date_start = date ("Y-m-d", strtotime("-7 day", strtotime($end_date)));
 //$end_date = date ("Y-m-d", strtotime("-1 day", strtotime($end_date)));
+$today = date("Y-m-d");
 $date_start = $_GET['fromDate'];
 $end_date = $_GET['toDate'];
 
@@ -21,37 +22,36 @@ while (strtotime($date_start) <= strtotime($end_date)) {
 }
 
 //TOP CP trong vong 1 tuan
-//$sql = "select name1,name2, sum(dau) from active_user_detail where type=6 and date_login >= '{$start_date}' AND date_login <= '{$end_date}' group by name1,name2 order by dau desc LIMIT 0,20";
+$sql = "select name1, sum(dau) from active_user_detail "
+        . "where type=6 and date_login >= '{$start_date}' AND date_login <= '{$end_date}' group by name1 order by dau desc LIMIT 0,20";
 //
 //
 //
 //
 ////die($sql);
-//$cps = array();
+$cps = array();
 //
 ////$cpname = array("x");
-//foreach ($db->query($sql) as $row) {
-//	$cps[] = 	"('{$row['name1']}','{$row['name2']}')";
+foreach ($db->query($sql) as $row) {
+	$cps[] = "'" . $row['name1'] . "'";
 //	//$cps[] = "'".$row['name1']."'";
 //
 //	//$cpname[] = $row['name1'];
-//}
+}
 //
 ////$cpname = asort($cpname);
 //
-//$cplist = implode(",",$cps);
+$cplist = implode(",",$cps);
 //
 //
 ////var_dump($cps);
 //
-//$sql1 = "select date_login,name1,name2, dau from active_user_detail 
-//			where type=6
-//					and (name1,name2) IN ({$cplist}) 
-//					and date_login >= '{$start_date}' AND date_login <= '{$end_date}'
-//				group by name1,name2,date_login 
-//				order by name1,name2"; //de duyet data dua vao mang cho de
-$sql1 = "select count(*) as dau, client_version as name1, date(last_login) as date_login from user "
-        . "where date(last_login) >= '{$start_date}' AND date(last_login) <= '{$end_date}' group by client_version, date(last_login)";
+$sql1 = "select date_login,name1, dau from active_user_detail 
+			where type=6
+					and (name1) IN ({$cplist}) 
+					and date_login >= '{$start_date}' AND date_login <= '{$end_date}'
+				group by name1,date_login 
+				order by name1"; //de duyet data dua vao mang cho de
 
 
 
@@ -70,7 +70,13 @@ foreach ($db->query($sql1) as $row) {
     //$data[] = array($row["date_login"],$row["name1"],$row["dau"]);
 }
 array_unshift($cpname, "x");
-
+if (in_array($today, $week)) {
+    $my_file = file_get_contents("./dau/rt_login_ver");
+    $jsonData = json_decode($my_file, true);
+    foreach ($jsonData as $val) {
+        array_push($data, $val);
+    }    
+}
 //echo json_encode($data);
 //var_dump($data);
 //asort($cpname);
