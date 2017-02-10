ff9fe688fb7c1613c45277a50882907c78b681cb
d4rksid3rs <gaproghe@gmail.com>
2017-02-10 19:57:12 +0700

    revshare complete

--- API/getRevShare.php
+++ API/getRevShare.php
@@ -55,6 +55,10 @@ try {
     $poker = $fee_array['POKER'];
     $ba_cay = $fee_array['BACAY'];
 
+    $total_fee_game = $tlmn + $bacaych + $tlmndc + $xoc_dia + $phom + $lieng + $sam
+            + $bau_cua + $xi_to + $poker + $ba_cay + $host_bau_cua + $host_xoc_dia;
+    $rev_share = ($total_fee_game + $total_card) * 0.81 + $total_sms * 0.45 + $total_iap * 0.65;
+    
     // render table
     $html = "<table width='100%'><tr style='background-color: rgb(255, 255, 255);font-weight:bold;'>";
     $html .= "<td width='5%'>STT</td><td width='30%'>Game</td><td>Fee</td></tr>";
@@ -69,12 +73,19 @@ try {
     $html .= "<tr><td>9</td><td>Xi to</td><td>" . number_format($xi_to) . "</td></td></tr>";
     $html .= "<tr><td>10</td><td>Poker</td><td>" . number_format($poker) . "</td></td></tr>";
     $html .= "<tr><td>11</td><td>Ba Cây</td><td>" . number_format($ba_cay) . "</td></td></tr>";
+    $html .= "<tr><td>12</td><td>Tiền Bầu Cua ăn</td><td>" . number_format($host_bau_cua) . "</td></td></tr>";
+    $html .= "<tr><td>13</td><td>Tiền Xóc đĩa ăn</td><td>" . number_format($host_xoc_dia) . "</td></td></tr>";
+    $html .= "<tr><td>14</td><td>Tổng <span style='font-weight:bold;color:red;'>(A)</span></td><td>" . number_format($total_fee_game) . "</td></td></tr>";
     $html .= "</table>";
-    echo $html;die;
-    var_dump($total_sms);
-    die;
-    echo $log;
-    die;
+    
+    $html .= "<br />";
+    $html .= "<table width='100%'><tr style='background-color: rgb(255, 255, 255);font-weight:bold;'>";
+    $html .= "<td width='5%'>STT</td><td width='30%'>Doanh thu từ Người chơi nạp Xu</td><td>$$$</td></tr>";
+    $html .= "<tr><td>1</td><td>SMS <span style='font-weight:bold;color:red;'>(B)</span></td><td>" . number_format($total_sms) . "</td></td></tr>";
+    $html .= "<tr><td>2</td><td>Card <span style='font-weight:bold;color:red;'>(C)</span></td><td>" . number_format($total_card) . "</td></td></tr>";
+    $html .= "<tr><td>3</td><td>IAP <span style='font-weight:bold;color:red;'>(D)</span></td><td>" . number_format($total_iap) . "</td></td></tr>";
+    $html .= "<tr><td>4</td><td>Tổng DT Chia sẻ <span style='font-weight:bold;color:red;'>[(A + C) x0.81 + B x 0.45 + D x 0.65]</span></td><td>" . number_format($rev_share) . "</td></td></tr>";
+    $html .= "</table>";       
 } catch (Exception $e) {
     echo $e->getMessage();
 }
