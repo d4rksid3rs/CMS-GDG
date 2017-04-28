e3d4e1e8bb8b6f344b1f4f2414061c0da8f407ba
d4rksid3rs <gaproghe@gmail.com>
2017-04-28 16:28:09 +0700

    fix server koin

--- API/get_server_koin.php
+++ API/get_server_koin.php
@@ -6,11 +6,13 @@ require('db.class.php');
 
 // Server Koin
 $today_timestamp = date("Y-m-d H:i:s");
-$sql_koin = "select sum(koin) as total, sum(koin_vip) as total_vip from auth_user";
+$sql_koin = "select sum(koin) as total_koin, sum(koin_vip) as total_vip_koin, sum(mkoin) as total_mkoin, sum(mkoin_vip) as total_vip_mkoin from auth_user";
 $stmt20 = $db->prepare($sql_koin);
 $stmt20->execute();
 foreach ($stmt20 as $row) {
-    $sql_insert = "INSERT INTO `server_koin` (`date`, `koin`, `koin_vip`) VALUES ('{$today_timestamp}', {$row['total']}, {$row['total_vip']})";
+    $total_koin = $row['total'] + $row['total_mkoin'];
+    $total_koin_vip = $row['total_vip'] + $row['total_vip_mkoin'];
+    $sql_insert = "INSERT INTO `server_koin` (`date`, `koin`, `koin_vip`) VALUES ('{$today_timestamp}', {$total_koin}, {$total_koin_vip})";
     $db->exec($sql_insert);
 }
 
